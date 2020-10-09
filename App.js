8827c22e87585fdabc291e434038762adde59345
DevsJabinho <douglasmb87@gmail.com>
2020-10-09 15:06:53 -0300

    09/10/2020

--- App.js
+++ App.js
@@ -1,50 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import * as Permissions from 'expo-permissions';
-import {Audio} from 'expo-av'
-import { View, StyleSheet, TextInput, Button,PermissionsAndroid } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet,Button } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 
-
-
-export default function App() {
-  useEffect(() => {
-    async function loadAudio() {
-      const { status } = await Audio.requestPermissionsAsync();
-
-      if (status !== 'granted') {
-        Alert.alert('Oooops...', 'Precisamos de sua permissão para obter o audio');
-        return;
-      } 
-
+const onButtonPress = () => {
+  console.log('foi apertado')
+  
+  };
+export default class app extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { webViewUrl: 'http://dese.gestaoci.com.br/?sys=shc&cli=2&seg=1' };
+  }
+_onMessage = event => {
+    console.log('_onMessage', JSON.parse(event.nativeEvent.data));
+    const res = JSON.parse(event.nativeEvent.data);
+    if (res.message === 'ok') {
+      alert('button clicked');
     }
+  };
+render() {
+  const jsCode = 'alert("teste")'
 
-    loadAudio();
-  }, []);
-  return(
-
-    <WebView
-    
-    originWhitelist={['*']}
-    allowsInlineMediaPlayback
-    
-     source={{uri:'http://dese.gestaoci.com.br/?sys=shm&cli=2&seg=1'}} style={{marginTop:20}}/>
-  )
+  
+    return (
+      <View style={styles.container}>
 
 
+        <WebView
+          ref={ref => {
+            this.webview = ref;
+          }}
+          source={{ uri: this.state.webViewUrl }}
+          originWhitelist={['*']}
+            
+          javaScriptEnabledAndroid={true}
+          injectedJavaScript={jsCode}
+          onMessage={this._onMessage}
+        />
+<Button
+onPress={onButtonPress}
+title='AperteME'
+color='#841584'
+accessibilityLabel='Learn more about this purple button'
+/>
 
+      </View>
 
+    );
+  }
 }
-
-
-
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
